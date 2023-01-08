7af2c90a9144c77cad702807d9565914a5b8e195
Fauzan Rizky <94896705+OjanRN@users.noreply.github.com>
2023-01-08 04:58:40 +0000

    configured flask templates

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
 
 @app.route('/<name>')
 def greet(name):
-    return f"Hello, {name}!"
+    return f"<h1>Hello, {name}!</h1>"
 
 if __name__ == "__main__":
     app.run()
